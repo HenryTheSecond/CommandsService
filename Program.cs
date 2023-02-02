232e80246cdf27615518bde7a0ad418036ae57e9
HenryTheSecond <congtuyen2032001@gmail.com>
2023-02-02 17:57:43 +0700

    aaa

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// builder.Services.AddCors(options => 
+// {
+//     options.AddPolicy(name: "PlatformService", policy =>
+//     {
+//         policy.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+//     });
+// });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +23,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// app.UseCors("PlatformService");
 
 app.UseAuthorization();
 
